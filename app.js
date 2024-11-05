5de79f8face7e9a2b1874fd0edc669e165f5eb85
Sushil <grayson.9999@gmail.com>
2024-11-05 21:12:07 +1000

    Implementing controller for home, login, and registration page

--- app.js
+++ app.js
@@ -21,17 +21,31 @@ console.log(__dirname, "====");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+// home page controller
+
 app.get("/", (req, res) => {
   console.log("req received");
   res.sendFile(__dirname + "/src/html/index.html");
 });
 
-app.get("/get-user", (req, res) => {
-  res.json({
-    fName: "Susil",
-    lName: "Dangoriya",
-  });
+// user registration controller
+app.get("/registration", (req, res) => {
+  console.log("req received registration");
+  res.sendFile(__dirname + "/src/html/register.html");
 });
+
+// user login controller
+app.get("/login", (req, res) => {
+  console.log("req received login");
+  res.sendFile(__dirname + "/src/html/login.html");
+});
+
+// app.get("/get-user", (req, res) => {
+//   res.json({
+//     fName: "Susil",
+//     lName: "Dangoriya",
+//   });
+// });
 app.listen(PORT, (error) => {
   error
     ? console.log(error)
