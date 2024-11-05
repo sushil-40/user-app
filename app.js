0bef3f8423afb50f1fdb2c9f73acca3c9d2cab09
Sushil <grayson.9999@gmail.com>
2024-11-05 20:52:42 +1000

    Adding some css property and implementing directory for server using path.resolve method

--- app.js
+++ app.js
@@ -3,13 +3,35 @@ const app = express();
 
 const PORT = 8000;
 
-app.get("/", (req, res) => {
-  //   console.log(req);
+// app.get("/", (req, res) => {
+//   //   console.log(req);
 
-  console.log("we got the request");
-  res.send("from server");
+//   console.log("we got the request");
+//   res.send(
+//     "<h1>Hello server dev</h1><hr/><p>lorem ipsum sfdksaflsafjdsajf    safsalfsalfjsa;fs    sfdsdflskfdl;sfdksaflsafjdsajf</p>"
+//   );
+// });
+
+import path from "path";
+
+const __dirname = path.resolve();
+console.log(__dirname, "====");
+
+// serve static file from the public directory
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  console.log("req received");
+  res.sendFile(__dirname + "/src/html/index.html");
 });
 
+app.get("/get-user", (req, res) => {
+  res.json({
+    fName: "Susil",
+    lName: "Dangoriya",
+  });
+});
 app.listen(PORT, (error) => {
   error
     ? console.log(error)
